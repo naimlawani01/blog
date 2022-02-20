74db9ce9c2070f85eb102a9d474a2ffd366749bd
Pythagore-byte <sekoubatoure300@gmail.com>
2022-02-20 23:00:15 +0800

    Page Inscription

--- connexion.php
+++ connexion.php
@@ -1,1 +1,20 @@
-i
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    <form action="">
+        <label for="email">Email:</label>
+        <input type="email" name="email" id="email"><br>
+        <label for="mdp">Mot de passe:</label>
+        <input type="password" name="mot_de_passe" id="mdp"><br>
+        <input type="submit" name="connexion" value="Connexion">
+ 
+    </form>
+    
+</body>
+</html>
---
+++ inscription.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Inscription</title>
+</head>
+<body>
+   <form action="">
+       <fieldset>
+           <legend>Informations Personnelles</legend>
+           <label for="nom">Nom: </label>
+           <input type="text" name="nom" id="nom"><br>
+           <label for="prenom">Prenom: </label>
+           <input type="text" name="prenom" id="prenom"><br>
+           <label for="email">Email: </label>
+           <input type="email" name="email" id="email"><br>
+           <label for="mdp">Mot de passe:</label>
+           <input type="password" name="mot_de_passe1" id="mdp"><br>
+           <label for="mdp">Confirmer:</label>
+           <input type="password" name="mot_de_passe2" id="mdp"><br>
+           <input type="submit" value="Envoyer">
+           
+           
+
+       </fieldset>
+       
+
+   </form>
+    
+</body>
+</html>
