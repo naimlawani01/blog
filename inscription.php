465d3859cb0bdf715b7dee450a0c9fd50c3869e7
Pythagore-byte <sekoubatoure300@gmail.com>
2022-02-21 00:49:47 +0800

    stockage des infos

--- inscription.php
+++ inscription.php
@@ -1,6 +1,26 @@
 <?php
-    if(isset($_POST['nom']) and isset($_POST['prenom']) and isset($_POST['email']) and isset($_POST['mot_de_passe1']) and isset($_POST['mot_de_passe2'])){
-        
+require('database.php');
+   //verifie si les champs sont saisis
+
+    if(isset($_POST['nom']) and isset($_POST['prenom']) and isset($_POST['email']) and isset($_POST['mot_de_passe1']) and isset($_POST['mot_de_passe2']))
+    {
+        $nom=$_POST['nom'];
+        $prenom=$_POST['prenom'];
+        $email=$_POST['email'];
+        $mdp1=$_POST['mot_de_passe1'];
+        $mdp2=$_POST['mot_de_passe2'];
+        if($mdp1==$mdp2)
+        {
+            $mdp =sha1($mdp1);
+            $insertion = $db->prepare('INSERT INTO users (nom, prenom, email,mot_de_passe) VALUES (?,?,?,?)');
+            $insertion->execute(array($nom, $prenom, $email,$mdp));
+
+        }
+        else
+        {
+            echo "Les deux(2) mots de passes ne sont pas identiques";
+        }
+
     }
 ?>
 
