6241ce22d99c1f28c12c903654264c1bfb79669b
Pythagore-byte <sekoubatoure300@gmail.com>
2022-02-21 23:01:48 +0800

    modification du code html

--- inscription.php
+++ inscription.php
@@ -30,30 +30,60 @@ require('database.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Inscription</title>
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;1,100&display=swap" rel="stylesheet">
+
 </head>
 <body>
-   <form method="POST" action="">
-       <fieldset>
-           <legend>Informations Personnelles</legend>
-           <label for="nom">Nom: </label>
-           <input type="text" name="nom" id="nom"><br>
-           <label for="prenom">Prenom: </label>
-           <input type="text" name="prenom" id="prenom"><br>
-           <label for="email">Email: </label>
-           <input type="email" name="email" id="email"><br>
-           <label for="mdp">Mot de passe:</label>
-           <input type="password" name="mot_de_passe1" id="mdp"><br>
-           <label for="mdp2">Confirmer:</label>
-           <input type="password" name="mot_de_passe2" id="mdp"><br>
-           <input type="submit" value="Envoyer">
-           
-           
+    <form action="" class="formu1">
+        <h1>Register</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas eaque blanditiis non repudiandae totam magnam autem quae eum unde, nihil tenetur dicta voluptate tempore dolorem natus consectetur asperiores! Ab, quasi?</p>
+        <div class="boite1">
+            <div class="gauche">
+                <label for="prenom">First Name</label><br>
+                <input type="text" name="prenom" id="prenom" placeholder="e.g John">
+            </div>
+            <div class="droite">
+                <label for="nom">Last Name</label><br>
+                <input type="text" name="nom" id="nom" placeholder="e.g Smith">
+            </div>
 
-       </fieldset>
-       
-
-   </form>
+        </div>
+        <div class="boite2">
+            <div class="milieu">
+                <label for="email">Email Adress</label><br>
+                <input class="email" type="text" id="email" name="email" placeholder="e.g john@your-domain.com">
+            </div>
+        </div>
+        <div class="boite3">
+            <div class="gauche">
+                <label for="numero">Phone Number</label><br>
+                <input type="text" name="prenom" id="numero" placeholder="+00 0000 000 0000">
+            </div>
+            <div class="droite">
+                <label for="siteweb">Website</label><br>
+                <input type="text" name="site" id="siteweb" placeholder="e.g https://google.com">
+            </div>
+        </div>
+        <div class="boite4">
+            <div class="gauche">
+                <label for="mdp1">Password</label><br>
+                <input type="password" name="mdp1" id="mdp1" placeholder="Your Password">
+            </div>
+            <div class="droite">
+                <label for="mdp2">Retype Password</label><br>
+                <input type="password" name="mdp2" id="mdp2" placeholder="Your Password">
+            </div>
+        </div>
+        <input type="submit" value="Enregister" name="envoyer">
+    </form>
+    
+    
     
 </body>
 </html>
